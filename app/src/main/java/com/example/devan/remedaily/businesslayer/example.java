3c8c23781784b186c7b6a71abdf3755f7131c220
DeepSingh93 <deep_prakash_singh@live.com>
2018-10-26 16:51:13 -0300

    3 layer architecture commit + sample test cases

---
+++ app/src/main/java/com/example/devan/remedaily/businesslayer/example.java
@@ -0,0 +1,8 @@
+package com.example.devan.remedaily.businesslayer;
+
+public class example {
+
+    public String DemoBusinessLayerFunction(){
+        return "This is a demo business layer function";
+    }
+}
---
+++ app/src/main/java/com/example/devan/remedaily/datalayer/example.java
@@ -0,0 +1,8 @@
+package com.example.devan.remedaily.datalayer;
+
+public class example {
+
+    public String DemoBusinessLayerFunction(){
+        return "This is a demo data layer function";
+    }
+}
