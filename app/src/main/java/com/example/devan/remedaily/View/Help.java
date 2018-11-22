c2f25b4d4e6b56708f2032f197df17bdb15a802a
AdityaGadhvi <adityagadhvi999@gmail.com>
2018-11-22 10:49:50 -0400

    Making some changes to the layout

--- app/src/main/java/com/example/devan/remedaily/View/Help.java
+++ app/src/main/java/com/example/devan/remedaily/View/Help.java
@@ -1,4 +1,4 @@
-package com.example.devan.remedaily;
+package com.example.devan.remedaily.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.devan.remedaily.R;
 
 public class Help extends AppCompatActivity {
 
