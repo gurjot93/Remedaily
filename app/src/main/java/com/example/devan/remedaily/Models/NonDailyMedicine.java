8abfda9ffd88dbd4992437044e973e201e1e3b2d
DeepSingh93 <deep_prakash_singh@live.com>
2018-11-26 16:28:15 -0400

    Complete restructure of calendar feature

---
+++ app/src/main/java/com/example/devan/remedaily/Models/NonDailyMedicine.java
@@ -0,0 +1,8 @@
+package com.example.devan.remedaily.Models;
+
+import java.util.ArrayList;
+
+public class NonDailyMedicine {
+    public String DateTime;
+    public ArrayList<Medicine> MedicineArrayList;
+}
--- app/src/main/java/com/example/devan/remedaily/datalayer/MedicineDataLayer.java
+++ app/src/main/java/com/example/devan/remedaily/datalayer/MedicineDataLayer.java
@@ -8,7 +8,10 @@
 
 package com.example.devan.remedaily.datalayer;
 
+import com.example.devan.remedaily.Models.Medicine;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MedicineDataLayer {
 
@@ -27,7 +30,19 @@ public class MedicineDataLayer {
 
     public ArrayList<Object[]> getMissedMedicineList() {
 
-        // return msised medicine details
+        // return missed medicine details
         return medicineDAOObject.getMissedMedicineDetails();
+    }
+
+    public ArrayList<String> getAllMedicineDates(){
+        return medicineDAOObject.getMedicineDates();
+    }
+
+    public ArrayList<Medicine> getMedicineFromDateTime(String DateObj){
+        return medicineDAOObject.getMedicineDataStubByDateTime(DateObj);
+    }
+
+    public ArrayList<Medicine> getDailyMedicineList(){
+        return medicineDAOObject.getDailyMedicineListDataStub();
     }
 }
